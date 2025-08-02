2cb3e9847442522c5b2ec4e81dd626266be3ede3
Alap Mistry <alapmistry23@gmail.com>
2025-08-02 12:16:27 +0530

    Add Java project with code quality GitLab CI pipeline

---
+++ src/main/java/com/demojava/Main.java
@@ -0,0 +1,12 @@
+package com.demojava;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+        System.out.println("Hello Alap!");
+        System.out.println("Hello Alap Mistry!");
+        System.out.println("Hello iBase-t!");
+        System.out.println("Hello Alap Mistry!");
+        System.out.println("Hello Alap!");
+    }
+}
